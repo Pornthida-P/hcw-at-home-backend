ad478d602373a3d03504268800dfc98d97e7ae0b
Pornthida-P <pornthida@convergence.co.th>
2026-03-04 14:54:01 +0700

    Update MongoDB connection URL in datastores configuration

--- config/datastores.js
+++ config/datastores.js
@@ -52,7 +52,7 @@ module.exports.datastores = {
     // url: 'mysql://user:password@host:port/database',
 
     adapter: 'sails-mongo',
-    url: process.env.DB_URI || 'mongodb://127.0.0.1:27019/hugathome?authSource=admin'
+    url: process.env.DB_URI || 'mongodb://127.0.0.1:27019/hcwathome?authSource=admin'
 
   }
 
